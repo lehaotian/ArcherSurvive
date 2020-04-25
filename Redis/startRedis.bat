298032e64f207b1a872eec75143a7375d793120f
乐浩天 <215034143@qq.com>
2020-04-25 11:53:25 +0800

    redis Signed-off-by: 乐浩天 <215034143@qq.com>

---
+++ Redis/startRedis.bat
@@ -0,0 +1,12 @@
+cd /d D:\egret\redis\Redis-7001
+start "7001" cmd /k call redisStart.bat
+cd /d D:\egret\redis\Redis-7002
+start "7002" cmd /k call redisStart.bat
+cd /d D:\egret\redis\Redis-7003
+start "7003" cmd /k call redisStart.bat
+cd /d D:\egret\redis\Redis-7004
+start "7004" cmd /k call redisStart.bat
+cd /d D:\egret\redis\Redis-7005
+start "7005" cmd /k call redisStart.bat
+cd /d D:\egret\redis\Redis-7006
+start "7006" cmd /k call redisStart.bat
